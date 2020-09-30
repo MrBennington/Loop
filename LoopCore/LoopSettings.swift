5b10ff422bf7c7cf92baedc2c726c0324a913d9a
MrBennington <27304217+MrBennington@users.noreply.github.com>
2020-09-30 12:34:00 +0200

    Update LoopSettings.swift

--- LoopCore/LoopSettings.swift
+++ LoopCore/LoopSettings.swift
@@ -38,7 +38,7 @@ public struct LoopSettings: Equatable {
 
     public let dynamicCarbAbsorptionEnabled = true
 
-    public static let defaultCarbAbsorptionTimes: CarbStore.DefaultAbsorptionTimes = (fast: .hours(2), medium: .hours(3), slow: .hours(4))
+    public static let defaultCarbAbsorptionTimes: CarbStore.DefaultAbsorptionTimes = (fast: .hours(1), medium: .hours(4), slow: .hours(7))
 
     public var glucoseTargetRangeSchedule: GlucoseRangeSchedule?
 
@@ -83,7 +83,7 @@ public struct LoopSettings: Equatable {
 
     public let minimumChartWidthPerHour: CGFloat = 50
 
-    public let statusChartMinimumHistoryDisplay: TimeInterval = .hours(1)
+    public let statusChartMinimumHistoryDisplay: TimeInterval = .hours(2)
     
     public var glucoseUnit: HKUnit? {
         return glucoseTargetRangeSchedule?.unit
